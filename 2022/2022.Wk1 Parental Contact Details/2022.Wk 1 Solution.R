2279ce9612d317eebfd58962684e108ad421d4be
Darragh Murray <darragh.murray@gmail.com>
2022-01-06 19:55:39 +1000

    Update data and initialise script

---
+++ 2022/2022.Wk1 Parental Contact Details/2022.Wk 1 Solution.R
@@ -0,0 +1,10 @@
+# PREPPIN' DATA | 2022 | Week 1 | The Prep School - Parental Contact Details 
+# https://preppindata.blogspot.com/2022/01/2022-week-1-prep-school-parental.html
+
+# INSTALL PACKAGES (if required)
+
+
+# INITIALISE LIBRARIES
+
+
+# IMPORT DATA
